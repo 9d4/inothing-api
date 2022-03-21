00782fb6ab6e80e117abae4369434abf4e456022
BayuDC <dev.bayudc@gmail.com>
2022-03-22 00:31:03 +0700

    add controller skeleton for auth

---
+++ app/auth/auth.controller.js
@@ -0,0 +1,15 @@
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+module.exports.login = (req, res) => {};
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+module.exports.logout = (req, res) => {};
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+module.exports.register = (req, res) => {};
