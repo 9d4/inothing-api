4788a7bbdc6355769560508d30246e5290552adb
BayuDC <dev.bayudc@gmail.com>
2022-03-22 07:53:16 +0700

    hash the user passsword

--- app/user/user.model.js
+++ app/user/user.model.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     fullname: {
@@ -22,4 +23,11 @@ const userSchema = new mongoose.Schema({
     },
 });
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.pre("save", async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+});
+
+module.exports = mongoose.model("User", userSchema);
