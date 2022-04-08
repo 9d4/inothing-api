11acc56a3ca07a2bf410512be6a813cc791a6cad
Dimas Ananda <traperwaze@gmail.com>
2022-04-08 18:44:57 +0700

    make sure no same thingId
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- app/thing/thing.schema.js
+++ app/thing/thing.schema.js
@@ -14,6 +14,16 @@ const thingSchema = module.exports = new mongoose.Schema({
 });
 
 thingSchema.pre("save", async function (next) {
-    // generate id then insert to thingId field
-    this.thingId = generateThingId();
+    let thingId = "";
+    let thing = null;
+
+    do {
+        // generate id then insert to thingId field
+        thingId = generateThingId();
+        // validate that there is no thing with the same thingId
+        thing = await this.constructor.findOne({ thingId });
+    } while (thing !== null);
+
+    this.thingId = thingId;
+    next();
 });
